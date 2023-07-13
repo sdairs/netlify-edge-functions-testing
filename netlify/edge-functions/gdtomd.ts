25c1527ad7b80504a7ca06e4087826cf35e06553
sdairs <sdairs@users.noreply.github.com>
2023-07-13 20:39:35 +0100

    log

--- netlify/edge-functions/gdtomd.ts
+++ netlify/edge-functions/gdtomd.ts
@@ -2,6 +2,7 @@ import type { Context } from "https://edge.netlify.com";
 
 export default async (request: Request, context: Context) => {
   const body = await request.text();
+  console.log(body);
   const response = await context.next();
   return new Response(body, response);
 };
