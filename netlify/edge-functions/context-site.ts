d0a941281445f6f7f174f9b6b4b3763df3fb18be
sdairs <sdairs@users.noreply.github.com>
2023-07-13 19:35:08 +0100

    remove shit

--- netlify/edge-functions/context-site.ts
+++
@@ -1,5 +0,0 @@
-import type { Context } from "https://edge.netlify.com";
-
-export default async (request: Request, context: Context) => {
-  return new Response(`Hello from ${context.site.name}!`);
-};
--- netlify/edge-functions/rewrite.ts
+++
@@ -1,5 +0,0 @@
-import type { Context } from "https://edge.netlify.com";
-
-export default async (request: Request, context: Context) => {
-  return new URL("/something-to-serve-with-a-rewrite", request.url);
-};
